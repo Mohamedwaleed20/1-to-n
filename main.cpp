adae5d22992088612628aa2fb1c80a2961d95be3
Mohamed waleed <Mohamed waleed@DESKTOP-0AUSUD8>
2024-07-16 14:24:18 +0300

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+int main()
+{
+    int n;
+    std::cin >> n ;
+    for(int i = 1 ; i <= n ; i++ )
+    {
+        std::cout << i << std::endl;
+    }
+
+    return 0;
+}
